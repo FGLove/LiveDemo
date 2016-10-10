a88580d9c19eb1d3e0cbfb4ef0455b00b7956937
FGLove <fugenchenfg@163.com>
2016-10-10 10:43:34 +0800

    搭建项目

---
+++ FGLiveDemo/FGLiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FGLiveDemo
+//
+//  Created by chfg on 16/10/10.
+//  Copyright © 2016年 chfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
